14a51d597f7aa4ccbb8cb810b547ff3e96d18c72
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-22 21:40:27 +0800

    +button A B --> go up

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(x, 2)
+    led.unplot(x, y)
     x += -1
-    led.plot(x, 2)
+    led.plot(x, y)
+})
+input.onButtonPressed(Button.AB, function () {
+    led.unplot(x, y)
+    y += -1
+    led.plot(x, y)
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(x, 2)
+    led.unplot(x, y)
     x += 1
-    led.plot(x, 2)
+    led.plot(x, y)
 })
+let y = 0
 let x = 0
 x = 2
-led.plot(x, 2)
+y = 2
+led.plot(x, y)
