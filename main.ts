892c21c52e29d5a96775c7f8caf8e2abe684a583
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-22 21:48:37 +0800

    let the light stay in the visible range

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
 input.onButtonPressed(Button.A, function () {
     led.unplot(x, y)
     x += -1
+    if (x == -1) {
+        x = 4
+    }
     led.plot(x, y)
 })
 input.onButtonPressed(Button.AB, function () {
     led.unplot(x, y)
     y += -1
+    if (y == -1) {
+        y = 4
+    }
     led.plot(x, y)
 })
 input.onButtonPressed(Button.B, function () {
     led.unplot(x, y)
     x += 1
+    if (x == 5) {
+        x = 0
+    }
     led.plot(x, y)
 })
 let y = 0
