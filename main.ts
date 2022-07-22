36f35a2ec1bdceb5e2fbf62229ebaa4b325d3ec1
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-22 22:14:12 +0800

    touch logo

--- main.ts
+++ main.ts
@@ -1,29 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    led.unplot(x, y)
-    x += -1
-    if (x == -1) {
-        x = 4
-    }
-    led.plot(x, y)
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    index += -1
 })
-input.onButtonPressed(Button.AB, function () {
-    led.unplot(x, y)
-    y += -1
-    if (y == -1) {
-        y = 4
-    }
-    led.plot(x, y)
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    index += 1
 })
-input.onButtonPressed(Button.B, function () {
-    led.unplot(x, y)
-    x += 1
-    if (x == 5) {
-        x = 0
-    }
-    led.plot(x, y)
+let index = 0
+basic.forever(function () {
+    basic.showNumber(index)
 })
-let y = 0
-let x = 0
-x = 2
-y = 2
-led.plot(x, y)
