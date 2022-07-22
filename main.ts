460d96243fc1235f70a77bccb0bf2bfd617a568f
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-22 21:25:29 +0800

    plot bar of (var) to (num) function

--- main.ts
+++ main.ts
@@ -1,14 +1,9 @@
 basic.forever(function () {
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            led.plot(x, y)
-            basic.pause(200)
-        }
-    }
-    for (let x = 0; x <= 4; x++) {
-        for (let y = 0; y <= 4; y++) {
-            led.toggle(y, x)
-            basic.pause(200)
-        }
+    for (let index = 0; index <= 10; index++) {
+        led.plotBarGraph(
+        index,
+        10
+        )
+        basic.pause(500)
     }
 })
