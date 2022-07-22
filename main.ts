d97ac4e4f2006a585f7563ba5942ac3624f7779c
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-22 21:17:25 +0800

    plot/toogle/unplug functions

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("Hello world!")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.plot(x, y)
+            basic.pause(200)
+        }
+    }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.toggle(y, x)
+            basic.pause(200)
+        }
+    }
 })
